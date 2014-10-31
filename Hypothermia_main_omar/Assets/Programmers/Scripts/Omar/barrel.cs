9181cc90abf073c736939ce878784fdd3332a4af
OmarPD <omardel23@comcast.net>
2014-10-31 15:16:35 -0700

    Here is my project

---
+++ Hypothermia_main_omar/Assets/Programmers/Scripts/Omar/barrel.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barrel : MonoBehaviour {
+
+
+	public GameObject Player;
+	public Health playerHealth;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+		Player = GameObject.Find("Player");
+		playerHealth = Player.GetComponent<Health>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	
+	}
+
+
+
+	void OnTriggerStay(Collider col)
+	{
+		if(col.gameObject.tag == "Player")
+		{
+			//print("HE HERE");
+			playerHealth.loseHealth = false;
+		}
+
+	}
+
+
+	void OnTriggerExit(Collider col)
+	{
+
+		if(col.gameObject.tag == "Player")
+		{
+			playerHealth.loseHealth = true;
+		}
+	}
+
+}
