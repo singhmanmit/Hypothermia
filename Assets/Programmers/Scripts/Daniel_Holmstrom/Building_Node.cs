2ced0540dbe0363de3a65d81d3d542ac3a9202e9
KillerFrogs <faytfrogy@yahoo.com>
2014-11-04 19:00:27 -0800

    Added Daniel's Stuff
    Added the building interior system that was made by Daniel Holmstrom.

---
+++ Assets/Programmers/Scripts/Daniel_Holmstrom/Building_Node.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Building_Node : MonoBehaviour {
+	public List<Interior_Node> Nodes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Programmers/Scripts/Daniel_Holmstrom/Interior_Node.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interior_Node : MonoBehaviour {
+	public bool IsWallObject;
+	public bool IsStandingObject;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
