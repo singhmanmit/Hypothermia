e7221be2d4342817ed818d799ca9331156c82b22
singh.manmit <manmit.singh408@gmail.com>
2014-11-04 20:54:21 -0800

    manis updates

---
+++ Assets/Programmers/Scripts/mani/flamethrower.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flamethrower : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		letPlay = false;
+	}
+	
+
+	protected bool letPlay;
+	
+	public void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			letPlay = !letPlay;
+		}
+
+		if(Input.GetMouseButtonDown(1))
+		{
+			letPlay = false;
+		}
+		
+		if(letPlay)
+		{
+			if(!gameObject.particleSystem.isPlaying)
+			{
+				gameObject.particleSystem.Play();
+			}
+		}else{
+			if(gameObject.particleSystem.isPlaying)
+			{
+				gameObject.particleSystem.Clear();
+			}
+		}
+	}
+}
