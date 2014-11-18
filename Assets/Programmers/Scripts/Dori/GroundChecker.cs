19f15c86b5ec03f4768fe23f96a76cbf3bb56e8b
Dori Chan <dori.p.chan@gmail.com>
2014-11-18 03:51:50 -0800

    -bad push (rewrote code)

--- Assets/Programmers/Scripts/Dori/GroundChecker.cs
+++ Assets/Programmers/Scripts/Dori/GroundChecker.cs
@@ -10,14 +10,14 @@ public class GroundChecker : MonoBehaviour
 
 	void OnTriggerStay(Collider other)
 	{
-		if(other.gameObject == GameObject.FindWithTag ("Obstacle")) {
+		if(other.gameObject == GameObject.FindWithTag ("Hut")) {
 			this.gameObject.transform.parent.GetComponent<SmallMonsterAI>().canJump = false;
 		}
 	}
 
 	void OnTriggerExit(Collider other) 
 	{
-		if(other.gameObject == GameObject.FindWithTag("Obstacle")) {
+		if(other.gameObject == GameObject.FindWithTag("Hut")) {
 			this.gameObject.transform.parent.GetComponent<SmallMonsterAI>().canJump = true;
 		}
 	}
