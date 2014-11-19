eebd621e57aee55d3a6fc71d22ae78ff81b6ebf7
KillerFrogs <faytfrogy@yahoo.com>
2014-11-18 19:59:37 -0800

    Created a rough script for placing the foundations of buildings on uneven terrain.

---
+++ Assets/Programmers/Scripts/Daniel_Holmstrom/Foundation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foundation : MonoBehaviour {
+	public Collider FloorCheck;
+	public GameObject FoundationBlock;
+
+	// Use this for initialization
+	void Start(){
+		if(FloorCheck==null){
+			Debug.Log("One of the buildings is missing the FloorCheck collider");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag=="Floor"){
+			GameObject.Instantiate(FoundationBlock, transform.position, transform.rotation);
+			gameObject.transform.position += new Vector3(0,2,0);
+		}
+	}
+
+}
